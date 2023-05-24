50111f3df62a178e5d51e8a41a298334469157dc
nijiko-ds <59920622+nijikos@users.noreply.github.com>
2023-05-24 14:18:15 +0700

    Create baseService

---
+++ src/services/baseService.ts
@@ -0,0 +1,119 @@
+import axios, { AxiosResponse } from "axios";
+
+interface requestGetProps<T> {
+  withAuth?: boolean;
+  params?: Record<string, unknown>;
+  token?: string;
+  needLogin?: boolean;
+}
+
+export const requestGet = async <T>(
+  url: string,
+  { withAuth = true, params, token, needLogin = true }: requestGetProps<T>
+): Promise<T> => {
+  try {
+    const response: AxiosResponse<T> = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}${url}`,
+      {
+        params,
+        headers: {
+          ...(withAuth && {
+            Authorization: `Bearer ${token}`,
+          }),
+        },
+      }
+    );
+
+    //@ts-ignore
+    if (response.statusCode === 401 || token === "") {
+    }
+    return response.data;
+  } catch (error: any) {
+    if (error.response.status === 401) {
+    }
+    throw error;
+  }
+};
+
+interface requestPostProps<T> {
+  withAuth?: boolean;
+  data?: T;
+  token?: string;
+  needLogin?: boolean;
+}
+
+export const requestPost = async <T>(
+  url: string,
+  {
+    withAuth = true,
+    data: requestBody,
+    token,
+    needLogin = true,
+  }: requestPostProps<T>
+): Promise<T> => {
+  try {
+    const response: AxiosResponse<T> = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}${url}`,
+      requestBody,
+      {
+        headers: {
+          ...(withAuth && {
+            Authorization: `Bearer ${token}`,
+          }),
+        },
+      }
+    );
+    console.log("###", { response, token });
+    //@ts-ignore
+    if (response.statusCode === 401 || token === "") {
+    }
+    return response.data;
+  } catch (error: any) {
+    if (error.response.status === 401) {
+    }
+    throw error;
+  }
+};
+
+export const requestLogout = async (message?: string) => {};
+
+// export const requestGet = async (url, { withAuth = true, params, needLogin = true, getErrorMessage }) => {
+//     try {
+//       const token = window.localStorage.getItem('authToken')
+//       const { data } = await axios.get(url, {
+//         params,
+//         headers: {
+//           ...(withAuth && {
+//             Authorization: `Bearer ${token}`
+//           })
+//         }
+//       })
+//       return data
+//     } catch (e) {
+//       const err = {
+//         status: e?.response?.status,
+//         statusText: e?.response?.data?.message
+//       }
+//       return err
+//     }
+//   }
+
+// export const requestPost = async (url, { withAuth = true, data: requestBody, needLogin = true, getErrorMessage }) => {
+//     try {
+//       const token = window.localStorage.getItem('authToken')
+//       const { data } = await axios.post(url, requestBody, {
+//         headers: {
+//           ...(withAuth && {
+//             Authorization: `Bearer ${token}`
+//           })
+//         }
+//       })
+//       return data
+//     } catch (e) {
+//       const err = {
+//         status: e?.response?.status,
+//         statusText: e?.response?.data?.message
+//       }
+//       return err
+//     }
+//   }
