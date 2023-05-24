4cfa585cb2bf723bebdbd550240d470c671488b9
nijiko-ds <59920622+nijikos@users.noreply.github.com>
2023-05-24 14:29:35 +0700

    Create Logo Component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type LogoProps = {
+  width?: string;
+};
+
+export default function Logo({ width }: LogoProps) {
+  return (
+    <div style={{ width: width ?? "140px" }}>
+      <img
+        src='/src/assets/images/logo/platter-logo-may.svg'
+        style={{ width: "100%" }}
+      />
+    </div>
+  );
+}
