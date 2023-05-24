c30efe182854b5c027ee789141f7f24632ff7f70
nijiko-ds <59920622+nijikos@users.noreply.github.com>
2023-05-24 14:05:17 +0700

    Create sign in page

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./pages/App";
+import Prompt from "./pages/Prompt";
 import Root from "./Root";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Root>
-      <App />
+      <Prompt />
     </Root>
   </React.StrictMode>
 );
