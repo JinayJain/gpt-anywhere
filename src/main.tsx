e179c4101bed150a7f6fdc00b60878bbfb90e16f
nijiko-ds <59920622+nijikos@users.noreply.github.com>
2023-05-24 14:05:46 +0700

    Temporarily set SignIn page as main

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Prompt from "./pages/Prompt";
+import SignIn from "./pages/SignIn";
 import Root from "./Root";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Root>
-      <Prompt />
+      {/* <Prompt /> */}
+      <SignIn />
     </Root>
   </React.StrictMode>
 );
