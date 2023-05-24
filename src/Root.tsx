9c553b872aab7ff64d3507ade809ddea800400de
nijiko-ds <59920622+nijikos@users.noreply.github.com>
2023-05-24 13:48:07 +0700

    Create tailwind-like css + import to root

--- src/Root.tsx
+++ src/Root.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import "@fontsource/manrope/500.css";
 import "@fontsource/manrope/700.css";
 import "./global.css";
+import "./tailwind.css";
 
 const Root = ({ children }: { children: React.ReactNode }) => {
   return (
